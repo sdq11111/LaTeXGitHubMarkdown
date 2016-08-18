3e40aef666cfc8fbeaf77babb2f29a17e93139ed
sdq11111 <451376709@qq.com>
2016-08-18 16:44:25 +0800

    检测逻辑。
    Signed-off-by: sdq11111 <451376709@qq.com>

--- popup.js
+++ popup.js
@@ -2,19 +2,29 @@
 /*global $, chrome, window*/
 $(document).ready(function () {
     'use strict';
-    $('#button_inject').on('click', function () {
-        chrome.tabs.query({active: true}, function (tabs) {
-            chrome.tabs.executeScript(tabs[0].id, {file: "render.js"});
-        });
-        window.close();
-    });
-    $('#button_access').on('click', function () {
-        chrome.tabs.create({url: 'https://github.com/login/oauth/authorize?client_id=17b967fc39122956bcc2'});
+    $.ajax({
+        url: 'http://www.baidu.com/',
+        success: function () {
+            $('#button_text').text('网络正常');
+            $('#status').attr('class', 'success');
+        },
+        error: function () {
+            $('#status').attr('class', 'fail');
+            $.ajax({
+                url: 'http://220.181.111.188/',
+                success: function () {
+                    $('#button_text').text('DNS解析异常');
+                },
+                error: function () {
+                    $('#button_text').text('无连接');
+                }
+            });
+        }
     });
     $('#button_github').on('click', function () {
-        chrome.tabs.create({url: 'https://github.com/CyberZHG/LaTeXGitHubMarkdown/'});
+        chrome.tabs.create({url: 'https://github.com/sdq11111/NetworkStatus/'});
     });
     $('#button_issues').on('click', function () {
-        chrome.tabs.create({url: 'https://github.com/CyberZHG/LaTeXGitHubMarkdown/issues/'});
+        chrome.tabs.create({url: 'https://github.com/sdq11111/NetworkStatus/issues/'});
     });
 });
